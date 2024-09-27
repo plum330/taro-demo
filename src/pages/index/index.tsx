573bbbb0f6eb60e295ce56a0d85c6450f3efc9cb
blackroot <smallfish_root@126.com>
2024-09-27 16:03:35 +0800

    feat: 加入浮动弹层

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,6 +4,7 @@ import '@/pages/index/index.scss'
 import PostCard from '@/components/PostCard'
 import { useState } from 'react'
 import PostForm from '@/components/PostForm'
+import { AtFab, AtFloatLayout, AtMessage } from 'taro-ui'
 
 export default function Index() {
 
@@ -22,12 +23,14 @@ export default function Index() {
 
   const [formTitle, setFormTitle] = useState('')
   const [formContent, setFormContent] = useState('')
+  // 记录浮动弹层是否打开
+  const [isOpened, setIsOpened] = useState(false)
 
   const handleSubmit = (e) => {
     setPosts(posts.concat({title: formTitle, content: formContent}))
     // 提交后是否清除之前输入的内容
-    // setFormTitle("")
-    // setFormContent("")
+    setFormTitle("")
+    setFormContent("")
   }
 
   return (
@@ -35,6 +38,7 @@ export default function Index() {
         {posts.map((post, index) => (
           <PostCard key={index} title={post.title} content={post.content} isList />
         ))}
+        <AtFloatLayout isOpened={isOpened} title='发表新文章' onClose={() => setIsOpened(false)}>
         <PostForm
           formTitle={formTitle}
           formContent={formContent}
@@ -42,6 +46,12 @@ export default function Index() {
           handleTitleInput={(e) => setFormTitle(e.target.value)}
           handleContentInput={(e) => setFormContent(e.target.value)}
         />
+        </AtFloatLayout>
+        <View className='post-button'>
+        <AtFab onClick={() => setIsOpened(true)}>
+          <Text className='at-fab__icon at-icon at-icon-edit'></Text>
+        </AtFab>
+      </View>
     </View>
   )
 }
